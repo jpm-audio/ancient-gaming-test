0aa13b0ac60f5c2d5717dcc16e596f3bb2e2e4bd
JesusPM <jesus@perezdemiguel.com>
2024-10-05 11:10:31 +0200

    Task 1: Added card counter to Deck class

--- src/components/deck.ts
+++ src/components/deck.ts
@@ -1,42 +1,89 @@
-import { Container, Point, PointData } from 'pixi.js';
+import { Container, Point, PointData, Text } from 'pixi.js';
 import Card from './card';
 
+type CardCounterPosition = 'top' | 'bottom';
+
 class Deck extends Container {
-  public CARD_OFFSET = { x: -1, y: -2 };
-  private cards: Card[] = [];
+  public ADD_CARD_OFFSET = { x: -1, y: -2 };
+  public COUNTER_GAP = 20;
+  private _cardsContainer: Container;
+  private _counter: Text;
+  private _counterPosition: CardCounterPosition;
 
   public get numCards() {
-    return this.cards.length;
+    return this._cardsContainer.children.length;
   }
 
-  constructor(offset: PointData = { x: 0, y: 0 }) {
+  public get counter() {
+    return this._counter;
+  }
+
+  public set counterGap(value: number) {
+    this.COUNTER_GAP = value;
+    this._positionCounter();
+  }
+
+  constructor(
+    addCardOffset: PointData = { x: 0, y: 0 },
+    CounterPosition: CardCounterPosition = 'top'
+  ) {
     super();
-    this.CARD_OFFSET = offset;
+    this.ADD_CARD_OFFSET = addCardOffset;
+    this._counterPosition = CounterPosition;
+
+    this._cardsContainer = new Container();
+    this.addChild(this._cardsContainer);
+
+    this._counter = new Text({
+      text: '0',
+      resolution: 1,
+      roundPixels: true,
+      anchor: 0.5,
+      style: {
+        fontFamily: 'Arial',
+        fontSize: 32,
+        fill: 0xffffff,
+        align: 'center',
+      },
+    });
+    this._counter.visible = false;
+    this.addChild(this._counter);
+  }
+
+  private _positionCounter() {
+    if (this._counter.visible) {
+      const cardHeight = this._cardsContainer.children.length
+        ? this._cardsContainer.getChildAt(0).height
+        : 0;
+      const direction = this._counterPosition === 'top' ? -1 : 1;
+      this._counter.y =
+        direction *
+        (this.COUNTER_GAP + (this._counter.height + cardHeight) / 2);
+    }
+  }
+
+  updateCounter() {
+    this._counter.visible = this.numCards > 0;
+
+    if (this._counter.visible) {
+      this._counter.text = this.numCards.toString();
+      this._positionCounter();
+    }
   }
 
   addCard(card: Card) {
-    this.cards.push(card);
-    this.addChild(card);
+    this._cardsContainer.addChild(card);
 
-    card.x = this.numCards * this.CARD_OFFSET.x;
-    card.y = this.numCards * this.CARD_OFFSET.y;
+    card.x = this.numCards * this.ADD_CARD_OFFSET.x;
+    card.y = this.numCards * this.ADD_CARD_OFFSET.y;
+
+    this.updateCounter();
   }
 
-  getCard(index: number = this.numCards - 1) {
-    const card = this.cards[index];
-
-    if (!card) {
-      throw Error(`Card ${index} not found`);
-    }
-
-    this.removeCard(card);
-
+  getCard(index: number = this.numCards - 1): Card {
+    const card = this._cardsContainer.removeChildAt<Card>(index);
+    this.updateCounter();
     return card;
-  }
-
-  removeCard(card: Card) {
-    this.cards.splice(this.cards.indexOf(card), 1);
-    this.removeChild(card);
   }
 }
 

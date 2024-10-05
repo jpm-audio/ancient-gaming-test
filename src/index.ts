b5c4d43db9b4d3f46e1ada92ae5d37211a016dca
JesusPM <jesus@perezdemiguel.com>
2024-10-05 11:11:06 +0200

    Task 1: Added GSAP as tween manager for animations

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { Application, Assets, Sprite } from 'pixi.js';
 import './style.css';
 import SceneTask1 from './scenes/task_1';
+import { gsap } from 'gsap';
+import { PixiPlugin } from 'gsap/PixiPlugin';
+import * as PIXI from 'pixi.js';
 
 (async () => {
+  // Init PixiJS
   const app = new Application();
-
   await app.init({ background: '#1099bb', resizeTo: window });
-
-  const scene = new SceneTask1();
-
-  await scene.init(app);
-
-  app.stage.addChild(scene);
-
   document.body.appendChild(app.canvas);
+
+  // Init GSAP
+  gsap.registerPlugin(PixiPlugin);
+  PixiPlugin.registerPIXI(PIXI);
+
+  // Init the scene
+  const scene = new SceneTask1();
+  await scene.init(app);
+  app.stage.addChild(scene);
 })();
--- src/scenes/task_1.ts
+++ src/scenes/task_1.ts
@@ -1,10 +1,11 @@
 import { Application, Assets, Container, Sprite } from 'pixi.js';
 import Deck from '../components/deck';
 import Card from '../components/card';
+import gsap from 'gsap';
 
 class SceneTask1 extends Container {
   private NUM_CARDS = 144;
-  private DECKS_GAP = 50;
+  private DECKS_GAP = 100;
   private decks: Deck[] = [];
 
   constructor() {
@@ -15,9 +16,9 @@ class SceneTask1 extends Container {
     // Load the assets
     const sheet = await Assets.load('assets/sprites/task_1_0.json');
 
-    // Create the deck of cards
-    this.decks.push(new Deck({ x: -1, y: -2 }));
-    this.decks.push(new Deck({ x: 1, y: 2 }));
+    // Create the decks of cards
+    this.decks.push(new Deck({ x: -1, y: -2 }, 'bottom'));
+    this.decks.push(new Deck({ x: 1, y: 2 }, 'bottom'));
 
     // Add the cards to the deck
     const cardFrameTexture = sheet.textures['card.png'];
@@ -43,11 +44,29 @@ class SceneTask1 extends Container {
     app.stage.addChild(this.decks[1]);
 
     const card = this.decks[0].getCard();
-    card.flip();
+    card.x = this.decks[0].x;
+    card.y = this.decks[0].y;
 
-    setTimeout(() => {
-      this.decks[1].addCard(card);
-    }, 1000);
+    app.stage.addChild(card);
+
+    const displacement = this.decks[1].x - this.decks[0].x;
+
+    gsap.to(card, {
+      x: card.x + displacement / 2,
+      duration: 3,
+      ease: 'power1.in',
+      onComplete: () => {},
+    }).vars.onComplete = () => {
+      card.flip();
+      gsap.to(card, {
+        x: this.decks[1].x,
+        duration: 3,
+        ease: 'power1.out',
+        onComplete: () => {
+          this.decks[1].addCard(card);
+        },
+      });
+    };
   }
 
   public async close() {}
